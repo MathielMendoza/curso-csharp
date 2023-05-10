cbadf29d3cd0a79b0a85cbda39fb1c0e1e6f9239
Gerson Mendoza <gerbo0906@gmail.com>
2023-05-09 22:08:18 -0600

    Added multiplication tables challenge

---
+++ 02-ControlDeFlujo/09-Reto-TablasDeMultiplicar/TablasDeMultiplicar/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TablasDeMultiplicar
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa un número");
+            string numero = Console.ReadLine();
+            int num = int.Parse(numero);
+
+            if (num >= 1 && num <= 10)
+            {
+                Console.WriteLine("La tabla de multiplicar de {0} es: ", num);
+
+                for (int i = 1; i <= 10; i++)
+                {
+                    int resultado = num * i;
+                    Console.WriteLine("{0} x {1} = {2}", num, i, resultado);
+                }
+            }
+            else
+            {
+                Console.WriteLine("El número ingresado debe estar entre 1 y 10");
+            }
+        }
+    }
+}
