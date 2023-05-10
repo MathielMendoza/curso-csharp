e1b571d09162e2b06ed4d593514b8df062abf8cb
Gerson Mendoza <gerbo0906@gmail.com>
2023-05-09 22:10:54 -0600

    Added class

---
+++ 03-Clases-Objetos/10-Clases/Clases/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Clases
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            //crear un objeto de tipo rectangulo
+
+            Rectangulo rect = new Rectangulo();
+            rect.Base = 10;
+            rect.Altura = 5;
+
+            int area = rect.CalcularArea();
+            Console.WriteLine("rect: {0} x {1}, area: {2}", rect.Base, rect.Altura, area);
+
+            // Sintaxis de inicialización
+            Rectangulo rect2 = new Rectangulo {Base = 3, Altura = 8};
+
+            int area2 = rect2.CalcularArea();
+            Console.WriteLine("rect: {0} x {1}, area: {2}", rect2.Base, rect2.Altura, area2);
+        }
+    }
+}
---
+++ 03-Clases-Objetos/10-Clases/Clases/Rectangulo.cs
@@ -0,0 +1,13 @@
+﻿namespace Clases
+{
+    public class Rectangulo
+    {
+        public int Base { get; set; }
+        public int Altura { get; set; }
+
+        public int CalcularArea()
+        {
+            return Base * Altura;
+        }
+    }
+}
