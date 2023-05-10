b6bfb6012d5eadc3c6cf1df6a5d4ee2fa6bf139e
Gerson Mendoza <gerbo0906@gmail.com>
2023-05-09 22:29:09 -0600

    Encapsulation: Added use of namespaces

---
+++ 04-ControlDeAcceso/16-Namespaces/Namespaces/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace MyNamespace
+{
+    class MyClass
+    {
+        public void MyMethod()
+        {
+            System.Console.WriteLine("Dentro de MyNameSpace.MyClass.MyMethod()");
+        }
+    }
+}
+
+namespace otherNamespace
+{
+    class MyClass
+    {
+        public void MyMethod()
+        {
+            System.Console.WriteLine("Dentro de otherNamespace.MyClass.MyMethod()");
+        }
+    }
+}
+
+namespace Namespaces
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Creando objetos de clases contenidas en otros namespaces
+            MyNamespace.MyClass myClass = new MyNamespace.MyClass();
+            otherNamespace.MyClass otherClass = new otherNamespace.MyClass();
+            
+            // Llamando a los metodos de las clases
+            myClass.MyMethod();
+            otherClass.MyMethod();
+        }
+    }
+}
