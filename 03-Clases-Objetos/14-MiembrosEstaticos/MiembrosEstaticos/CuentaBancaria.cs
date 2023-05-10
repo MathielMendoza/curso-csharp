829b87dc3a28c816014515683e46581069c074fc
Gerson Mendoza <gerbo0906@gmail.com>
2023-05-09 22:16:13 -0600

    Added static members

---
+++ 03-Clases-Objetos/14-MiembrosEstaticos/MiembrosEstaticos/CuentaBancaria.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace MiembrosEstaticos
+{
+    public class CuentaBancaria
+    {
+        public string NoCuenta { get; set; }
+        public string Usuario { get; set; }
+        private decimal _saldo;
+
+        public decimal Saldo
+        {
+            get { return _saldo; }
+            set { _saldo = value < 0 ? 0 : value; }
+        }
+
+        public static string Banco { get; set; }
+
+        #region constructores
+
+        public CuentaBancaria(string noCuenta)
+        {
+            NoCuenta = noCuenta;
+        }
+
+        public CuentaBancaria(string noCuenta, string usuario) : this(noCuenta)
+        {
+            Usuario = usuario;
+        }
+
+        public CuentaBancaria(string noCuenta, string usuario, decimal saldo) : this(noCuenta, usuario)
+        {
+            Saldo = saldo;
+        }
+
+        public CuentaBancaria()
+        {
+        }
+
+        #endregion
+
+        #region Métodos de instancia
+
+        public void Retirar(decimal cantidad)
+        {
+            if (cantidad > Saldo)
+            {
+                Console.WriteLine("Saldo Insuficiente");
+                return;
+            }
+
+            Saldo -= cantidad;
+        }
+
+        public void Depositar(decimal cantidad)
+        {
+            Saldo += cantidad;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("NoCuenta: {0}, Usuario: {1}, Saldo: {2}", NoCuenta, Usuario, Saldo);
+        }
+
+        #endregion
+
+        public static void AsignarBanco(string banco)
+        {
+            Console.WriteLine("Asignado nuevo banco...");
+            Banco = banco;
+        }
+    }
+}
---
+++ 03-Clases-Objetos/14-MiembrosEstaticos/MiembrosEstaticos/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MiembrosEstaticos
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Para acceder a los miembros estáticos se usa solo el nombre de la clase
+            CuentaBancaria cuenta1 = new CuentaBancaria("001", "John Doe", 100);
+            CuentaBancaria cuenta2 = new CuentaBancaria("002", "Jane Doe", 250);
+
+            CuentaBancaria.Banco = "Mi Banco";
+
+            Console.WriteLine(cuenta1);
+            Console.WriteLine(cuenta2);
+        }
+    }
+}
