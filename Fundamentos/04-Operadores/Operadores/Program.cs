aa452a150abcdee86a0becfee2574b12c9225492
Gerson Mendoza <gerbo0906@gmail.com>
2023-05-09 21:34:49 -0600

    Added operators

---
+++ Fundamentos/04-Operadores/Operadores/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Operadores
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a = 30;
+            int b = 11;
+
+            // Operadores aritméticos
+            Console.WriteLine(a + b);
+            Console.WriteLine(a - b);
+            Console.WriteLine(a * b);
+            Console.WriteLine(a / b);
+            Console.WriteLine(a % b); // módulo
+            bool par = a % 2 == 0;
+            Console.WriteLine(par);
+
+            // Operadores unarios
+            a++; // a = a + 1;
+            a--; // a = a - 1 
+
+            // Operadores de comparación
+            Console.WriteLine(a > b);
+            Console.WriteLine(a >= b);
+            Console.WriteLine(a < b);
+            Console.WriteLine(a <= b);
+            Console.WriteLine(a == b);
+            Console.WriteLine(a != b);
+        }
+    }
+}
