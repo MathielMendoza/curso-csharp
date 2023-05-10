88c4e57bb911d3574e80fb6d0f5454149b032bd6
Gerson Mendoza <gerbo0906@gmail.com>
2023-05-09 22:12:49 -0600

    Added properties

---
+++ 03-Clases-Objetos/12-Propiedades/Propiedades/CuentaBancaria.cs
@@ -0,0 +1,54 @@
+﻿namespace Propiedades
+{
+    public class CuentaBancaria
+    {
+        //Auto properties (propiedades automáticas)
+        public string NoCuenta { get; set; }
+        public string Usuario { get; set; }
+
+        //backing field
+        private decimal _saldo;
+
+        //Full property (propiedad completa)
+        public decimal Saldo
+        {
+            get { return _saldo; }
+            set
+            {
+                //evitar que el saldo sea negativo
+                /*if (value < 0)
+                {
+                    _saldo = 0;
+                }
+                else
+                {
+                    _saldo = value;
+                }*/
+                _saldo = value < 0 ? 0 : value;
+            }
+        }
+
+        #region constructores
+
+        public CuentaBancaria(string noCuenta)
+        {
+            NoCuenta = noCuenta;
+        }
+
+        public CuentaBancaria(string noCuenta, string usuario) : this(noCuenta)
+        {
+            Usuario = usuario;
+        }
+
+        public CuentaBancaria(string noCuenta, string usuario, decimal saldo) : this(noCuenta, usuario)
+        {
+            Saldo = saldo;
+        }
+
+        public CuentaBancaria()
+        {
+        }
+
+        #endregion
+    }
+}
---
+++ 03-Clases-Objetos/12-Propiedades/Propiedades/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Propiedades
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            CuentaBancaria cuenta = new CuentaBancaria();
+
+            cuenta.Saldo = -5;
+            Console.WriteLine("Saldo después de primera asignación: {0}", cuenta.Saldo);
+            
+            cuenta.Saldo = 10;
+            Console.WriteLine("Saldo después de primera asignación: {0}", cuenta.Saldo);
+        }
+    }
+}
