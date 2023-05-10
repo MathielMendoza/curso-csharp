adb3baeb9f5670550461037394db4f911502ceea
Gerson Mendoza <gerbo0906@gmail.com>
2023-05-09 22:02:16 -0600

    Added read keyboard

---
+++ 02-ControlDeFlujo/08-LeerTeclado/LeerTeclado/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LeerTeclado
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Suma de dos números");
+            Console.WriteLine("Ingresa el número 1: ");
+            //Los datos se leen como cadena
+            string dato = Console.ReadLine();
+            // Convertir a número int.Parse
+
+            int n1 = int.Parse(dato);
+
+            Console.WriteLine("Ingresa el número 2: ");
+            dato = Console.ReadLine();
+            int n2 = int.Parse(dato);
+
+            int suma = n1 + n2;
+            Console.WriteLine("El resultado es: {0}", suma);
+        }
+    }
+}
