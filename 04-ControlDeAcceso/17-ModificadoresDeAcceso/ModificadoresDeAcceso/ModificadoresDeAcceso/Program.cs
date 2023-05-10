af659ae58d8ae9ec2b45b1684678195a008823c5
Gerson Mendoza <gerbo0906@gmail.com>
2023-05-09 22:44:07 -0600

    Encapsulation: Added access modifiers

---
+++ 04-ControlDeAcceso/17-ModificadoresDeAcceso/ModificadoresDeAcceso/ModificadoresDeAcceso/Program.cs
@@ -0,0 +1,17 @@
+﻿using OtroNamespaces;
+
+namespace ControlAcceso
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            MiClase obj1 = new MiClase();
+            
+            obj1.MetodoPublico();
+            obj1.MetodoInternal();
+            //obj1.MetodoProtected();
+            //obj1.MetodoPrivate();
+        }
+    }
+}
