61fdcb8042404ce19f4d9d7f628ce3f62767d4de
Gerson Mendoza <gerbo0906@gmail.com>
2023-05-09 21:57:49 -0600

    Added loops-2

---
+++ 02-ControlDeFlujo/07-Ciclos2/Ciclos2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Ciclos2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Uso de break");
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 5)
+                {
+                    break; // termina con el ciclo
+                }
+
+                Console.WriteLine("Valor del contador i: {0}", i);
+            }
+
+            Console.WriteLine("\nUso de continue");
+            int n = 0;
+            while (n < 10)
+            {
+                if (n % 2 == 0)
+                {
+                    n++;
+                    continue;
+                }
+
+                Console.WriteLine("Valor del contador n: {0}", n);
+                n++;
+            }
+        }
+    }
+}
