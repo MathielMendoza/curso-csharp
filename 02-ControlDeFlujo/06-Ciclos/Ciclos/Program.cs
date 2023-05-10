0fa3487e962f6b2e4ea3a54e6ec8b0cf8521a763
Gerson Mendoza <gerbo0906@gmail.com>
2023-05-09 21:53:43 -0600

    Added loops

---
+++ 02-ControlDeFlujo/06-Ciclos/Ciclos/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Ciclos
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Ciclo For
+
+            Console.WriteLine("Usando ciclo For");
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Valor del contador i: {0}", i);
+            }
+
+            //Ciclo While
+            Console.WriteLine("\nUsando ciclo While");
+            int n = 0;
+            while (n < 5)
+            {
+                Console.WriteLine("Valor del contador i: {0}", n);
+                n++;
+            }
+
+            //Ciclo Do - While
+            Console.WriteLine("\nUsando ciclo Do - While");
+            int c = 0;
+            do
+            {
+                Console.WriteLine("Valor del contador c: {0}", c);
+                c++;
+            } while (c < 5);
+        }
+    }
+}
