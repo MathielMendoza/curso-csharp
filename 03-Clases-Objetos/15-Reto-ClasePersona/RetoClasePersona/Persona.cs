091407510dd9b6dc98d486c1368683caefa19ce3
Gerson Mendoza <gerbo0906@gmail.com>
2023-05-09 22:18:07 -0600

    Added person class challenge

---
+++ 03-Clases-Objetos/15-Reto-ClasePersona/RetoClasePersona/Persona.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.SqlServer.Server;
+
+namespace RetoClasePersona
+{
+    public class Persona
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        
+        private int _edad;
+        public int Edad
+        {
+            get { return _edad; }
+            set { _edad = value >= 0 ? value : 0; }
+        }
+
+        #region Constructores
+
+        public Persona(string nombre)
+        {
+            Nombre = nombre;
+        }
+
+        public Persona(string nombre, string apellido) : this(nombre)
+        {
+            Apellido = apellido;
+        }
+
+        public Persona(string nombre, string apellido, int edad) : this(nombre, apellido)
+        {
+            Edad = edad;
+        }
+
+        public Persona()
+        {
+        }
+
+        #endregion
+
+        #region Métodos
+
+        public void Saludar()
+        {
+            Console.WriteLine("Hola mi nombre es {0} {1}", Nombre, Apellido);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Nombre: {0}, Apellido: {1}, Edad: {2} ", Nombre, Apellido, Edad);
+        }
+
+        #endregion
+    }
+}
---
+++ 03-Clases-Objetos/15-Reto-ClasePersona/RetoClasePersona/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RetoClasePersona
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Persona p1 = new Persona("John","Due",30);
+            Persona p2 = new Persona("Jane","Due",40);
+            
+            p1.Saludar();
+            p2.Saludar();
+
+            p1.Edad = -10;
+            Console.WriteLine(p1);
+        }
+    }
+}
